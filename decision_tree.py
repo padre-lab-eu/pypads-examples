ae04b2cac632caf4f11b3d8965234bd174ef66be
Thomas Weißgerber <weissger@posteo.de>
2020-10-19 13:59:45 +0200

    Initialized examples git repo

---
+++ decision_tree.py
@@ -0,0 +1,40 @@
+"""
+===========================================
+DecisionTree Classification on Iris dataset
+===========================================
+An example of using PyPads to track different functions and classes used in a minimal classification examples.
+"""
+import os
+
+from pypads import logger
+from pypads.app.base import PyPads
+
+path = os.path.expanduser('~')
+
+tracker = PyPads(uri="git:/{}/.pypads/results".format(path), autostart=True)
+# tracker.backend.add_result_remote("origin", "ssh://git@gitlab.padim.fim.uni-passau.de:13003/Mehdi/pypads-results-testt.git")
+
+
+from sklearn import datasets
+from sklearn.metrics.classification import f1_score
+from sklearn.tree import DecisionTreeClassifier
+
+# load the iris datasets
+dataset = datasets.load_iris()
+print("branch test")
+
+# fit a model to the data
+model = DecisionTreeClassifier()
+model.fit(dataset.data, dataset.target)
+# make predictions
+expected = dataset.target
+predicted = model.predict(dataset.data)
+# summarize the fit of the model
+logger.error("Score: " + str(f1_score(expected, predicted, average="macro")))
+
+
+print(tracker.api.get_artifacts())
+print(tracker.api.search_artifacts(search=".*json"))
+print(tracker.api.list_logger_calls())
+
+tracker.api.end_run()
